14da1d92ccbdb32b39d823f4c744783bbd463d44
makhosi6 <ndondo330@gmail.com>
2022-12-04 01:14:02 +0200

    Sunday 12/04/2022 01:14 - add logo and branding

--- js/main.js
+++ js/main.js
@@ -182,13 +182,13 @@ center: true,
   });
 
   // filter items on button click
-  $('.portfolio-menu').on('click', 'button', function () {
+  $('.canonical-menu').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
   });
 
   //for menu active class
-  $('.portfolio-menu button').on('click', function (event) {
+  $('.canonical-menu button').on('click', function (event) {
     $(this).siblings('.active').removeClass('active');
     $(this).addClass('active');
     event.preventDefault();
